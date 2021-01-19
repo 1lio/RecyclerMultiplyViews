ea8479291d8323133e483e09438500c6f91b3f8c
ViacheslavSukhov <svo_efremov@mail.ru>
2021-01-19 16:25:42 +0300

    Created ViewHolderFactory

---
+++ app/src/main/java/org/sample/view/factory/ViewHolderFactory.kt
@@ -0,0 +1,41 @@
+package org.sample.view.factory
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.recyclerview.widget.RecyclerView
+import org.sample.R
+import org.sample.view.rows.Row
+
+object ViewHolderFactory {
+
+    class TextViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView = view.findViewById(R.id.textView)
+    }
+
+    class ImageViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val imgView: AppCompatImageView = view.findViewById(R.id.imageView)
+    }
+
+    class ImageTextViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView = view.findViewById(R.id.textView)
+        val imgView: AppCompatImageView = view.findViewById(R.id.imageView)
+    }
+
+    fun create(parent: ViewGroup, itemType: Int): RecyclerView.ViewHolder {
+
+        return when (itemType) {
+            Row.TEXT -> TextViewHolder(R.layout.card_1.inflate(parent))
+            Row.IMAGE -> ImageViewHolder(R.layout.card_2.inflate(parent))
+            Row.IMAGE_AND_TEXT -> ImageTextViewHolder(R.layout.card_3.inflate(parent))
+
+            else -> TextViewHolder(R.layout.card_1.inflate(parent))
+        }
+    }
+
+    private fun Int.inflate(parent: ViewGroup) =
+        LayoutInflater.from(parent.context).inflate(this, parent, false)
+
+}
